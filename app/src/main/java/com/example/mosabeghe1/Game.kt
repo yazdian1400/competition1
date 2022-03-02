a63e8b1b1a2bdfd9b2424c1393ba24625d68e4ed
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-03-02 20:28:14 +0330

    maxScore / Random Choice / reset game

--- app/src/main/java/com/example/mosabeghe1/Game.kt
+++ app/src/main/java/com/example/mosabeghe1/Game.kt
@@ -1,10 +1,13 @@
 package com.example.mosabeghe1
 
+import java.lang.Math.max
+
 object Game {
     var level = 1
     var score = 0
     var a = 0
     var b = 0
+    var maxScore = -20
 
     fun dice() {
         a = (0..100).random()
@@ -32,4 +35,27 @@ object Game {
             score -= 2
         }
     }
+    fun generateAllChoicesRandomly (): MutableList<Int>{
+        val correctChoice = generateCorrectChoice()
+        val max = max(b, 4)
+        val list = (0 until max).toMutableList()
+        list.remove(correctChoice)
+        list.shuffle()
+        val choices = list.slice(0 .. 2).toMutableList()
+        choices.add(correctChoice)
+        choices.shuffle()
+        return choices
+
+    }
+    fun reset(){
+        level = 1
+        score = 0
+        a = 0
+        b = 0
+    }
+
+    fun setMaximumScore(score: Int){
+        if (score > maxScore) maxScore = score
+    }
+
 }
