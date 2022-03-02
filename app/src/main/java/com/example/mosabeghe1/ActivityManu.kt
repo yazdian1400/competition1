86f88539617c4e857e47b17bbffd5510dbe52dd4
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-03-02 22:04:04 +0330

    add activity menu

---
+++ app/src/main/java/com/example/mosabeghe1/ActivityManu.kt
@@ -0,0 +1,11 @@
+package com.example.mosabeghe1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityManu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
