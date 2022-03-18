22f57aedd45bd8030a9b2c2e64b7b3836de48703
Maedeh <yazdian.oul@gmail.com>
2022-03-18 20:04:12 +0330

    add code for ResultFragment.kt

--- app/src/main/java/com/example/mosabeghe1/ResultFragment.kt
+++ app/src/main/java/com/example/mosabeghe1/ResultFragment.kt
@@ -1,10 +1,12 @@
 package com.example.mosabeghe1
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.mosabeghe1.databinding.FragmentResultBinding
 
 
@@ -21,5 +23,27 @@ class ResultFragment : Fragment() {
     ): View? {
         binding = FragmentResultBinding.inflate(layoutInflater, container, false)
         return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var score = 0
+        if (arguments?.getInt("score",0) != null) {
+            score = arguments?.getInt("score", 0)!!
+        }
+        Game.setMaximumScore(score)
+        binding.textViewMaxScore.text = Game.maxScore.toString()
+        binding.tvShowScore.text = " ${score.toString()}"
+        binding.btnExit.setOnClickListener{
+            requireActivity().finishAffinity()
+        }
+        binding.btnNewGame.setOnClickListener{
+            Game.reset()
+            resetGame()
+        }
+    }
+    fun resetGame() {
+        findNavController().navigate(R.id.action_resultFragment_to_settingFragment)
     }
 }
